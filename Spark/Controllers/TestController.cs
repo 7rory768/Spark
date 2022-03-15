74ca94fe86ca4cca31508e47c2482f0fbb7fd0c3
Rory Skipper <roryskipper123@gmail.com>
2022-03-14 22:26:00 -0600

    Skeleton

---
+++ Spark/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Spark.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "GetRandomNumber")]
+        public int GetRandomNumber()
+        {
+            return Random.Shared.Next(0, 1000);
+        }
+    }
+}
